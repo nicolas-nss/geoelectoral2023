0bf17d9d69e3dcbd47e19b836de3579ef43919d5
Nicolás Sánchez Salazar <nicolas.sanchoz15@gmail.com>
2025-05-29 00:04:12 -0500

    Add files via upload

---
+++ mapa/styles/14MARTIRES_27_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_14MARTIRES_27 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Arial\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 4.5;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("PVONSITIO") !== null) {
+        labelText = String(feature.get("PVONSITIO"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 7.2 + size, points: 4,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(50,87,128,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(39,173,216,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
